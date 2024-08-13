65fbdae931ac64b46d002277489f72e1c2072e8d
BHC-Chicken <tij05078@gmail.com>
2024-08-14 03:34:51 +0900

    [fix] 게시글 유저 이메일 누락 수정

--- src/main/java/dev/ioexception/community/entity/Article.java
+++ src/main/java/dev/ioexception/community/entity/Article.java
@@ -2,11 +2,14 @@ package dev.ioexception.community.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedAttributeNode;
+import jakarta.persistence.NamedEntityGraph;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,13 +17,17 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@NamedEntityGraph(
+        name = "Article.user",
+        attributeNodes = @NamedAttributeNode("user")
+)
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
     @Column(name = "image_url")
